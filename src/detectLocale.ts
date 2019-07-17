1857188d9bc2eeb69b15217a4d3e6e7cd00d4db4
skt-t1-byungi <tiniwz@gmail.com>
2019-07-17 21:25:49 +0900

    fix detecLocale

--- src/detectLocale.ts
+++ src/detectLocale.ts
@@ -1,14 +1,18 @@
 import find = require('@skt-t1-byungi/array-find')
 
 export default function detectLocale (locales: string[]) {
-    let lang: string | void
+    let lang: string | undefined
     if (process && process.env) {
         lang = process.env.LC_ALL || process.env.LC_MESSAGES || process.env.LANG || process.env.LANGUAGE
     } else if (navigator) {
-        lang = navigator.language || (navigator as (Navigator & { userLanguage: string })).userLanguage
+        lang = (navigator.language || (navigator as (Navigator & { userLanguage: string })).userLanguage)
     }
 
-    if (!lang) return 'unknown'
+    if (lang) {
+        lang = lang.toLowerCase()
+        const found = find(locales, str => lang!.match(str.toLowerCase()) !== null)
+        if (found) return found
+    }
 
-    return find(locales, str => (lang as string).match(str) !== null) || lang
+    return locales.length > 1 ? locales[0] : 'unknown'
 }
